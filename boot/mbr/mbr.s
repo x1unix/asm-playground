a3cc72fff217a57da0d3d04af1f3409e15be74ce
x1unix <denis0051@gmail.com>
2019-11-22 00:37:43 +0200

    text printing works :tada:

--- boot/mbr/mbr.s
+++ boot/mbr/mbr.s
@@ -1,29 +1,29 @@
-; start loop
-mov cl, 0
+MBR_MEM_OFFSET equ 0x7c00 ; BIOS MBR memory start offset
+EOL equ 0 ; NUL terminator
 
 mov ah, 0x0e ; tty mode
-print:
-mov al, 'F'
-int 0x10
-mov al, 'U'
-int 0x10
-mov al, 'C'
-int 0x10
-mov al, 'K'
-int 0x10
-mov al, ' '
-int 0x10
+mov cx, 0 ; start loop
 
-inc cl
-cmp cl, 3 ; if (cx <= 3)
-jle print
+mov bx, msg
+add bx, MBR_MEM_OFFSET ; 0x7c00
+
+print:
+    mov al, [bx]    ; Take value from bx address to al
+    int 0x10        ; Print interrupt
+    inc bx          ; Increate pointer to get next char
+    mov dl, [bx]
+    cmp dl, EOL
+    jne print
 
 mov al, '!'
 int 0x10
 int 0x10
 int 0x10
 
-jmp $ ; jump to current address = infinite loop
+;jmp $ ; jump to current address = infinite loop
+
+msg:
+    db 10, 10, "!!! Oh god, it works !!!", 0
 
 ; padding and magic number
 times 510 - ($-$$) db 0
