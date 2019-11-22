291f3a4fa6ee534eb83bd09216f5af33bab1c323
x1unix <denis0051@gmail.com>
2019-11-22 06:23:39 +0200

    add clear screen

--- src/mbr/mbr.s
+++ src/mbr/mbr.s
@@ -1,13 +1,22 @@
 [org 0x7c00]    ; Bootloader location as global memory offset
 
-mov bx, msg     ; Put string address to bx
+call cls
+
+mov bx, hello     ; Put string address to bx
 call print      ; Call custom "print" subroutine
+
+mov bx, msg
+call print
+
 jmp $           ; jump to current address = infinite loop
 
 %include "print.s"
 
+hello:
+    db "Hello World", CR, LF, EOL
+
 msg:
-    db CR, LF, "!!! OMG, it works !!!", EOL
+    db "Wake up, neo", CR, LF, EOL
 
 ; padding and magic number
 times 510 - ($-$$) db 0
--- src/mbr/print.s
+++ src/mbr/print.s
@@ -5,6 +5,20 @@ LF  equ 0x0d        ; Carriage return (LF)
 
 MODE_TTY equ 0x0e   ; BIOS TTY text mode
 
+; cls clears (or tries at least) the screen
+cls:
+    pusha
+
+    mov ax, 0x0700  ; function 07, AL=0 means scroll whole window
+    mov bh, 0x07    ; character attribute = white on black
+    mov cx, 0x0000  ; row = 0, col = 0
+    mov dx, 0x184f  ; row = 24 (0x18), col = 79 (0x4f)
+    int 0x10        ; call BIOS video interrupt
+
+    popa
+    ret
+
+
 ; Print prints the string to stdout
 ;
 ; Parameters:
